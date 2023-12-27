a2f7b23a1db7a5c0963b6b71844ff889ac65892f
Icaro Rodrigues <icaro.rodrigues@protonmail.com>
2023-12-27 17:59:29 -0300

    change empty provider block

--- versions.tf
+++ versions.tf
@@ -1,3 +1,14 @@
 terraform {
   required_version = ">= 0.12.0"
+
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      configuration_aliases = [
+        aws.peer,
+      ]
+
+      version = ">= 4.0"
+    }
+  }
 }
